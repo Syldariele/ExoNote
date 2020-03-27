6cb6704f0ef583981acf1b18ab54d8365172495b
Sitael <beru666@live.fr>
2020-03-27 11:56:17 +0100

    correction fichier.sql

--- fichier.sql
+++ fichier.sql
@@ -1,9 +1,22 @@
 /* CREATION DE LA TABLE ARTICLES */
 
-CREATE TABLE node_articles
-(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, title VARCHAR(100), content VARCHAR(256), author INT, created_at DATETIME);
+CREATE TABLE node_articles (
+                               id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                               title VARCHAR(128) NOT NULL,
+                               content VARCHAR(256) NOT NULL,
+                               author INT NOT NULL,
+                               created_at DATETIME NOT NULL,
+                               FOREIGN KEY (author) REFERENCES node_users(id)
+);
 
 /* CREATION DE LA TABLE COMMENTS */
 
-CREATE TABLE node_comments
-(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, article_id INT, author INT, content VARCHAR(256), created_at DATETIME);
+CREATE TABLE node_comments (
+                               id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                               article_id INT NOT NULL,
+                               content VARCHAR(256),
+                               author INT NOT NULL,
+                               created_at DATETIME,
+                               FOREIGN KEY (article_id) REFERENCES node_articles(id),
+                               FOREIGN KEY (author) REFERENCES node_users(id)
+);
