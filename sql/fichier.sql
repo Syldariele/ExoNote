4a25cb79977264e5ea630c273c37305e442c696e
Sitael <beru666@live.fr>
2020-04-01 09:31:12 +0200

    correction

--- sql/fichier.sql
+++ sql/fichier.sql
@@ -22,3 +22,26 @@ CREATE TABLE node_comments (
                                FOREIGN KEY (article_id) REFERENCES node_articles(id), /* constraint FK_comments_article_id */
                                FOREIGN KEY (author) REFERENCES node_users(id) /*FK_comments_author */
 );
+
+/* Correction */
+
+-- create table articles
+CREATE TABLE node_articles (
+                               id INT PRIMARY KEY AUTO_INCREMENT,
+                               title VARCHAR(100) NOT NULL,
+                               content TEXT NOT NULL,
+                               author INT NOT NULL,
+                               created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                               CONSTRAINT FK_articles_author FOREIGN KEY (author) REFERENCES node_users(id)
+);
+
+-- create table comments
+CREATE TABLE node_comments (
+                               id INT PRIMARY KEY AUTO_INCREMENT,
+                               articles_id INT NOT NULL,
+                               author INT NOT NULL,
+                               content TEXT NOT NULL,
+                               created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                               CONSTRAINT FK_comments_articles_id FOREIGN KEY (articles_id) REFERENCES node_articles(id) ON DELETE CASCADE,
+                               CONSTRAINT FK_comments_author FOREIGN KEY (author) REFERENCES node_users(id)
+);
