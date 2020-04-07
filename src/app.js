eb6e91ab9bcadd74fc6528939fb183a6fa427559
Sitael <beru666@live.fr>
2020-04-07 17:01:37 +0200

    add res.header on app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const app = express();
 app.listen(3001, () => {
     console.log("SERVEUR STARTED !");
 });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true}));
