0a16109530bf0998f346f5f7fdbcbb18d3ccdbbc
Sitael <beru666@live.fr>
2020-04-09 14:15:41 +0200

    replace req.body.article_id by req.body.id

--- src/api/comments.js
+++ src/api/comments.js
@@ -64,14 +64,14 @@ app.get("/api/comments", (req, res) => {
     const sqlConnection = mysql.createConnection(sqlConfig);
 
     sqlConnection.query(
-        "SELECT articles_id, content, node_users.firstname AS authorFirstname, node_users.lastname AS authorLastname, created_at"
+        "SELECT article_id, content, node_users.firstname AS authorFirstname, node_users.lastname AS authorLastname, created_at"
         + "  FROM node_comments"
         + "  LEFT JOIN node_users"
         + "  ON node_comments.author = node_users.id"
-        + "  WHERE articles_id = ?"
+        + "  WHERE article_id = ?"
         + "  ORDER BY created_at DESC"
         + "  LIMIT 5;",
-        [ req.query.articles_id ],
+        [ req.query.id ],
         (error, result) => {
             if (error) {
                 console.log(error.code);
